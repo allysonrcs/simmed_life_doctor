e28e37cc049823a28cd3212ae5158d292f347a55
allysonrcs <allysonrcs@gmail.com>
2025-06-23 14:07:20 -0300

    Reorganize pages and routes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useNotification } from '../context/NotificationContext.jsx'
+import { useNotification } from '../../context/NotificationContext.jsx'
 
 export default function Home() {
   const navigate = useNavigate()
